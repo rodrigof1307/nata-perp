17e2e1696ec997654e39885dff11ec69edd5959f
rodrigof1307 <rodrigo.fernandes.1307@gmail.com>
2023-11-03 17:39:40 +0000

    feat: frontend setup

---
+++ trading-perp-frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-center font-bold text-orange-600 underline">
+        Hello world, I am Trading Perp
+      </h1>
+      <Button>And I am a Button with shadcn UI</Button>
+    </main>
+  );
+}
